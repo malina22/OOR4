8e01a0d7b7d7964b73928fc15cf896b55bb2112d
Kamil <m.kowalski767@wp.pl>
2016-01-20 22:39:25 +0100

    oor44

--- Asynchronicznosc/WpfAsync/MainWindow.xaml.cs
+++ Asynchronicznosc/WpfAsync/MainWindow.xaml.cs
@@ -26,25 +26,25 @@ namespace WpfAsync
             InitializeComponent();
         }
 
-        private async void buttonAsync_Click(object sender, RoutedEventArgs e)
+        private async void buttonAsync_Click(object sender, RoutedEventArgs e) // guzik działający asynchronicznie
         {
-            buttonAsync.IsEnabled = false;
+            buttonAsync.IsEnabled = false; // guzik działajacy asynchronicznie ustawiony na wartosc false
             
-            var slowTask = Task<string>.Factory.StartNew(() => SlowDude());
+            var slowTask = Task<string>.Factory.StartNew(() => SlowDude()); // zmienna potrzeba do asynchronicznosci
             
-            textBoxResults.Text += "Spokojnie chlopie zaraz odpowiem\r\n";            
+            textBoxResults.Text += "Spokojnie chlopie zaraz odpowiem\r\n"; // Pierwsza odpowiedz po wcisnieciu guzika            
             
-            await slowTask;            
+            await slowTask;             
 
-            textBoxResults.Text += slowTask.Result.ToString();
+            textBoxResults.Text += slowTask.Result.ToString(); //rezultat
             
             buttonAsync.IsEnabled = true;
         }
 
-        private string SlowDude()
+        private string SlowDude() //zmienna prywatna
         {
-            Thread.Sleep(2000);
-            return "Czesc to ja ;) !\r\n";
+            Thread.Sleep(2000); //watek zadziała po 2 sekundach 
+            return "Czesc to ja ;) !\r\n"; // odpowiedz
         }      
         
     }
